c09ba34102560273ff929271c4e41828f0a545e0
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-12-19 11:33:12 -0500

    added semantic  ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
      <h1>theVBook</h1>
+     <Button>Click here</Button>
     </div>
   );
 }
