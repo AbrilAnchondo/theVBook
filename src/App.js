4945676e2ad4d9c771861dddd3e4b68799acedbe
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-12-20 01:54:56 -0500

    added reach router and first route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Router, Link } from '@reach/router';
+import Home from './components/Home';
 import RecipeContainer from './components/RecipeContainer';
 
 
-
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <RecipeContainer />
+    <div>
+      <nav>
+        <Link to="/">Home</Link>{" "}
+        <Link to="recipes">Recipes</Link>
+      </nav>
+      <Router>
+        <Home path="/" />
+        <RecipeContainer path="/recipes" />
+      </Router>
     </div>
   );
 }
