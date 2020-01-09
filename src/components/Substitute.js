b9e6a99b90068eaeed18539e6e39ffbbce1df4dd
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-01-09 01:57:47 -0500

    added Substitute component, gets substitute for a given ingredient

--- src/components/Substitute.js
+++ src/components/Substitute.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Substitute = () => {
+
+const Substitute = ({ ingredients }) => {
+  const [input, setInput] = useState('');
+  const [substitutes, setSubstitutes] = useState([]);
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setInput(event.target.value); 
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch(`https://api.spoonacular.com/food/ingredients/substitutes?ingredientName=${input}&apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}`);
+    const subs = await response.json();
+    console.log("Subs", subs);
+    setSubstitutes(subs.substitutes);
+  }
+
+  console.log("state substitutes", substitutes);
+
   return (
-    <h2>Need a Substitute?</h2>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Need a substitute?
+          <input type='text' 
+            name='name' 
+            value={input}
+            onChange={handleChange} />
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
   )
 }
 
