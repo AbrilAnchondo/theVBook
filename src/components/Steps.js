6d203f120507948b14dc49b6c85bb9ce5fac74bc
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-02-02 18:46:59 -0500

    Cleaned up styling Steps list

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,16 +1,22 @@
 import React, { Fragment } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, List } from 'semantic-ui-react';
 
 const Steps = ({ instructions }) => {
   //console.log("instructions",instructions);
   return (
     <Fragment>
-      <Header as='h2' dividing>Steps</Header>
-      <ul>
-        {instructions.map((step, index) => <li key={index}>{step}</li>)}
-      </ul>
+      <Header as='h2' dividing>Follow these Steps:</Header>
+      <List ordered>
+        {instructions.map((step, index) => <List.Item key={index}>{step}</List.Item>)}
+      </List>
     </Fragment>
   )
 }
 
 export default Steps
+    // <Fragment>
+    //   <Header as='h2' dividing>Steps</Header>
+    //   <ul>
+    //     {instructions.map((step, index) => <li key={index}>{step}</li>)}
+    //   </ul>
+    // </Fragment>
