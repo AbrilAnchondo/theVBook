65f2628eb88e6639f3c31767e1afc80fccf3d832
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-02-09 14:18:00 -0500

    added cuisine, intolerances and diet filter with semantic ui styling

--- src/components/AllRecipes.js
+++ src/components/AllRecipes.js
@@ -7,25 +7,45 @@ import '../index.css';
 const AllRecipes = () => {
   const [recipes, setRecipes] = useState([]);
   const [cuisine, setCuisine] = useState('');
+  const [diet, setDiet] = useState('');
+  const [intolerances, setIntolerances] = useState('');
 
   useEffect(() => {
     const fetchRecipes = async () => { 
-      const response = await fetch(`https://api.spoonacular.com/recipes/search?cuisine=${cuisine}&diet=vegan&number=20&apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}`);
+      const response = await fetch(`https://api.spoonacular.com/recipes/search?cuisine=${cuisine}&diet=${diet}&intolerances=${intolerances}&number=20&apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}`);
       const data = await response.json();
       const results = data.results;
       setRecipes(results);
      
     }
     fetchRecipes();
-  }, [cuisine]);
+  }, [intolerances]);
 
-  const onCuisineChange = (e) => {
-    setCuisine(e.target.value);
+  const onCuisineChange = (e, { value }) => {
+    console.log(value);
+    setCuisine(value);
+  }
+
+  const onDietChange = (e, { value }) => {
+    console.log(value);
+    setDiet(value);
+  }
+  
+  const onIntolerancesChange = (e, { value }) => {
+    console.log(value);
+    setIntolerances(value);
   }
 
   return (
     <Fragment>
-      <Options cuisine={cuisine} onCuisineChange={onCuisineChange}/>
+      <Options 
+        cuisine={cuisine} 
+        onCuisineChange={onCuisineChange} 
+        diet={diet} 
+        onDietChange={onDietChange}
+        intolerances={intolerances}
+        onIntolerancesChange={onIntolerancesChange}
+      />
       <div className="grid recipe-list">
           { recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe} />) }
       </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,94 +1,88 @@
-import React, { Fragment, useState } from 'react';
+import React from 'react';
 
 import { Dropdown, Grid } from 'semantic-ui-react';
 
-// const cuisineOptions = [
-//   { key: 'af', value: 'af', text: 'African'},
-//   { key: 'am', value: 'am', text: 'American'},
-//   { key: 'br', value: 'br', text: 'British'},
-//   { key: 'caj', value: 'caj', text: 'Cajun'},
-//   { key: 'car', value: 'car', text: 'Caribbean'},
-//   { key: 'ch', value: 'ch', text: 'Chinese'},
-//   { key: 'ee', value: 'ee', text: 'Easter European'},
-//   { key: 'eu', value: 'eu', text: 'European'},  
-//   { key: 'fr', value: 'fr', text: 'French'},
-//   { key: 'ge', value: 'ge', text: 'German'},
-//   { key: 'gr', value: 'gr', text: 'Greek'},  
-//   { key: 'in', value: 'in', text: 'Indian'},  
-//   { key: 'ir', value: 'ir', text: 'Irish'},  
-//   { key: 'it', value: 'it', text: 'Italian'},  
-//   { key: 'ja', value: 'ja', text: 'Japanese'},
-//   { key: 'je', value: 'je', text: 'Jewish'},  
-//   { key: 'ko', value: 'ko', text: 'Korean'},  
-//   { key: 'la', value: 'la', text: 'Latin American'},
-//   { key: 'me', value: 'me', text: 'Mediterranean'},  
-//   { key: 'mex', value: 'mex', text: 'Mexican'},  
-//   { key: 'med', value: 'med', text: 'Middle Eastern'},
-//   { key: 'no', value: 'no', text: 'Nordic'},  
-//   { key: 'so', value: 'so', text: 'Southern'},  
-//   { key: 'sp', value: 'sp', text: 'Spanish'},  
-//   { key: 'th', value: 'th', text: 'Thai'},  
-//   { key: 'vi', value: 'vi', text: 'Vietnamese'}              
-// ];
+const cuisineOptions = [
+  { key: 'af', value: 'African', text: 'African'},
+  { key: 'am', value: 'American', text: 'American'},
+  { key: 'br', value: 'British', text: 'British'},
+  { key: 'caj', value: 'Cajun', text: 'Cajun'},
+  { key: 'car', value: 'Caribbean', text: 'Caribbean'},
+  { key: 'ch', value: 'Chinese', text: 'Chinese'},
+  { key: 'ee', value: 'Eastern European', text: 'Eastern European'},
+  { key: 'eu', value: 'European', text: 'European'},  
+  { key: 'fr', value: 'French', text: 'French'},
+  { key: 'ge', value: 'German', text: 'German'},
+  { key: 'gr', value: 'Greek', text: 'Greek'},  
+  { key: 'in', value: 'Indian', text: 'Indian'},  
+  { key: 'ir', value: 'Irish', text: 'Irish'},  
+  { key: 'it', value: 'Italian', text: 'Italian'},  
+  { key: 'ja', value: 'Japanese', text: 'Japanese'},
+  { key: 'je', value: 'Jewish', text: 'Jewish'},  
+  { key: 'ko', value: 'Korean', text: 'Korean'},  
+  { key: 'la', value: 'Latin American', text: 'Latin American'},
+  { key: 'me', value: 'Mediterranean', text: 'Mediterranean'},  
+  { key: 'mex', value: 'Mexican', text: 'Mexican'},  
+  { key: 'med', value: 'Middle Eastern', text: 'Middle Eastern'},
+  { key: 'no', value: 'Nordic', text: 'Nordic'},  
+  { key: 'so', value: 'Southern', text: 'Southern'},  
+  { key: 'sp', value: 'Spanish', text: 'Spanish'},  
+  { key: 'th', value: 'Thai', text: 'Thai'},  
+  { key: 'vi', value: 'Vietnamese', text: 'Vietnamese'}              
+];
 
-// const dietOptions = [
-//   { key: 'vgn', value: 'vgn', text: 'Vegan'},
-//   { key: 'veg', value: 'veg', text: 'Vegetarian'},
-//   { key: 'lveg', value: 'lveg', text: 'Lacto Vegetarian'},
-//   { key: 'oveg', value: 'oveg', text: 'Ovo-Vegetarian'},
-// ];
+const dietOptions = [
+  { key: 'vgn', value: 'Vegan', text: 'Vegan'},
+  { key: 'veg', value: 'Vegetarian', text: 'Vegetarian'},
+  { key: 'lveg', value: 'Lacto-Vegetarian', text: 'Lacto-Vegetarian'},
+  { key: 'oveg', value: 'Ovo-vegetarian', text: 'Ovo-Vegetarian'},
+];
 
-// const intoleranceOptions = [
-//   { key: 'da', value: 'da', text: 'Diary'},
-//   { key: 'eg', value: 'eg', text: 'Egg'},
-//   { key: 'gl', value: 'gl', text: 'Gluten'},
-//   { key: 'gr', value: 'gr', text: 'Grain'},
-//   { key: 'pe', value: 'pe', text: 'Peanut'},
-//   { key: 'se', value: 'se', text: 'Sesame'},
-//   { key: 'so', value: 'so', text: 'Soy'},
-//   { key: 'su', value: 'su', text: 'Sulfite'},
-//   { key: 'tn', value: 'tn', text: 'Tree Nut'},
-//   { key: 'wh', value: 'wh', text: 'Wheat'}
-// ];
+const intolerancesOptions = [
+  { key: 'da', value: 'Dairy', text: 'Diary'},
+  { key: 'eg', value: 'Egg', text: 'Egg'},
+  { key: 'gl', value: 'Gluten', text: 'Gluten'},
+  { key: 'gr', value: 'Grain', text: 'Grain'},
+  { key: 'pe', value: 'Peanut', text: 'Peanut'},
+  { key: 'se', value: 'Sesame', text: 'Sesame'},
+  { key: 'so', value: 'Soy', text: 'Soy'},
+  { key: 'su', value: 'Sulfite', text: 'Sulfite'},
+  { key: 'tn', value: 'Tree Nut', text: 'Tree Nut'},
+  { key: 'wh', value: 'Wheat', text: 'Wheat'}
+];
 
 const Options = (props) => {
 
   return (
-      <form>
-        <label>
-          Select Cuisine Type: 
-          <select value={props.cuisine} onChange={props.onCuisineChange} >
-            <option value=''>Choose an option</option>
-            <option value='African'>African</option>
-            <option value='American'>American</option>
-            <option value='British'>British</option>
-            <option value='Cajun'>Cajun</option>
-            <option value='Caribbean'>Caribbean</option>
-            <option value='Chinese'>Chinese</option>
-            <option value='Easter European'>Easter European</option>
-            <option value='European'>European</option>
-            <option value='French'>French</option>
-            <option value='German'>German</option>
-            <option value='Greek'>Greek</option>
-            <option value='Indian'>Indian</option>
-            <option value='Irish'>Irish</option>
-            <option value='Italian'>Italian</option>
-            <option value='Japanese'>Japanese</option>
-            <option value='Jewish'>Jewish</option>
-            <option value='Korean'>Korean</option>
-            <option value='Latin American'>Latin American</option>
-            <option value='Mediterranean'>Mediterranean</option>
-            <option value='Mexican'>Mexican</option>
-            <option value='Middle Eastern'>Middle Eastern</option>
-            <option value='Nordic'>Nordic</option>
-            <option value='Southern'>Southern</option>
-            <option value='Spanish'>Spanish</option>
-            <option value='Thai'>Thai</option>
-            <option value='Vietnamese'>Vietnamese</option>
-          </select>
-        </label>  
-      </form>
+    <Grid columns={3}>
+      <Grid.Column>
+        <Dropdown
+          onChange={props.onCuisineChange}
+          options={cuisineOptions}
+          placeholder='Select Cuisine'
+          value={props.cuisine}
+        />
+      </Grid.Column>
+      <Grid.Column>
+        <Dropdown
+          onChange={props.onDietChange}
+          options={dietOptions}
+          placeholder='Select Diet'
+          value={props.diet}
+        />
+      </Grid.Column>
+      <Grid.Column>
+        <Dropdown
+          onChange={props.onIntolerancesChange}
+          options={intolerancesOptions}
+          placeholder='Select Intolerance'
+          value={props.intolerances}
+        />
+      </Grid.Column>
+    </Grid>
+
     )
   }
   
   export default Options
+
