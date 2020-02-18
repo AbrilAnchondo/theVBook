6488dc0b8783541e5a4445562c735e8bcd4c7b9c
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-02-17 23:35:06 -0500

    added post route to /auth to log in user

---
+++ routes/api/auth.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+//route post   api/auth
+//description  long in
+//access       public
+router.post('/', [
+   check('email', 'Please include a valid email').isEmail()
+],
+async (req,res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+  const { email } = req.body;
+
+  try {
+    let user = await User.findOne({ email })
+
+    //see if user exists
+    if (!user) {
+      return res.status(400).json({ errors: [ { msg: 'Invalid credentials'}]})
+    }
+    
+    res.send('User registered');
+  }catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server error')
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -18,5 +18,6 @@ app.get('/', (req, res) => res.send('API is running...'));
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/recipes', require('./routes/api/recipes'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
