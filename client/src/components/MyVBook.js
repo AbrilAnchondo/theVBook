d701e72ea52975d58692cba3bd1de0266e6e8ec7
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-05-22 23:32:52 -0400

    displayed recipes for a given user

--- client/src/components/MyVBook.js
+++ client/src/components/MyVBook.js
@@ -1,19 +1,63 @@
-import React, { Fragment } from 'react';
-import { Divider, Image } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
 
-const MyVBook = () => {
+import RecipeList from './RecipeList';
+import axios from 'axios';
+//import { Divider, Image } from 'semantic-ui-react';
+
+const MyVBook = (props) => {
   //console.log("props",props.location.state.image)
   //const src = props.location.state.image;
+  let userId = localStorage.userId;
+  const [userRe, setUserRe] = useState([]);
+  const [recipeList, setRecipeList] = useState([]);
+  
+  useEffect (() => {
+    const fetchUserRecipes = async () => {
+    
+      const configObj = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accepts': 'application/json'
+        }
+      };
+      const response = await axios.get(`http://localhost:5000/api/users/${userId}`, configObj);
+      console.log('response: ',response);
+      const userRe = response.data;
+      console.log('userRe :',userRe)
+      setUserRe(userRe);
+      let ids = userRe.map(re => re.recipeID).join();
+      console.log('ids: ',ids);
+      const res = await fetch(`https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&includeNutrition=false&apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}`);
+      console.log(res);
+      const recipeList = await res.json();
+      //console.log(recipeList);
+      setRecipeList(recipeList);
+    } 
+    fetchUserRecipes();
+ },[])
+
+  // useEffect(() => {
+  //   //console.log(userRe);
+  //   let ids = userRe.map(re => re.recipeID).join();
+  //   console.log('ids: ',ids);
+  //     const fetchRecipesFromAPI = async () => {
+  //     const res = await fetch(`https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&includeNutrition=false&apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}`);
+  //     console.log(res);
+  //     const recipeList = await res.json();
+  //     //console.log(recipeList);
+  //     setRecipeList(recipeList);
+  //     //console.log('state recipeList: ',recipeList);
+  //     }
+  //     fetchRecipesFromAPI();
+  // },[userRe])  
+  
+  //console.log(recipeList);
   return (
-    <Fragment>
       <div>
-        <Image src=""/>
-        <h2>recipe name</h2>
-        <h4>go to recipe</h4>
-        <Divider />
+        <h1>User's saved recipes</h1>
+          <RecipeList recipes={recipeList} />
       </div>
-    </Fragment>
   )
 }
 
-export default MyVBook
+export default MyVBook 
