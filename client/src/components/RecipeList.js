f14fc71ec3fedceeddc6c717d75c397c4b77d855
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-05-22 23:34:54 -0400

    added reusable component RecipeList

---
+++ client/src/components/RecipeList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Recipe from './Recipe';
+
+const RecipeList = (props) => {
+  console.log('RecipeList props:', props);
+  const recipes = props.recipes;
+  console.log('recipes: ',recipes);
+  return (
+    <div className="grid recipe-list">
+      { recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe} />) }
+    </div>
+  )
+}
+
+export default RecipeList
