21774862a0257402c6bf02431e543b16c3f9448f
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-05-27 17:12:27 -0400

    merged user recipes and api recipe into single array of recipes

--- client/src/components/MyVBook.js
+++ client/src/components/MyVBook.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-import RecipeList from './RecipeList';
+//import RecipeList from './RecipeList';
 import axios from 'axios';
+import SavedReList from './SavedReList';
+//import { set } from 'mongoose';
 //import { Divider, Image } from 'semantic-ui-react';
 
 const MyVBook = (props) => {
@@ -21,25 +23,41 @@ const MyVBook = (props) => {
         }
       };
       const response = await axios.get(`http://localhost:5000/api/users/${userId}`, configObj);
-      console.log('response: ',response);
+      //console.log('response: ',response);
       const userRe = response.data;
       console.log('userRe :',userRe)
       setUserRe(userRe);
       let ids = userRe.map(re => re.recipeID).join();
-      console.log('ids: ',ids);
-      const res = await fetch(`https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&includeNutrition=false&apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}`);
-      console.log(res);
+      //console.log('ids: ',ids);
+      const res = await fetch(`https://api.spoonacular.com/recipes/informationBulk?ids=${ids}&includeNutrition=true&apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}`);
+      //console.log(res);
       const recipeList = await res.json();
-      //console.log(recipeList);
+      console.log('recipeList: ',recipeList);
       setRecipeList(recipeList);
     } 
     fetchUserRecipes();
  },[])
 
+ const mergedList = (arr1,arr2) => {
+  const temp = [];
+  arr1.forEach(x => {
+    arr2.forEach(y => {
+      if(x.recipeID == y.id) {
+        temp.push({ ...x, ...y})
+      }
+    })
+  })
+  console.log('temp: ',temp)
+  return temp
+ }
+ let fullReList = mergedList(userRe,recipeList);
+ console.log('fullReList: ',fullReList);
+
   return (
       <div>
-        <h1>User's saved recipes</h1>
-          <RecipeList recipes={recipeList} />
+        <h1>Your Recipes</h1>
+        <SavedReList details={fullReList} />
+        {/* <RecipeList recipes={recipeList} /> */}
       </div>
   )
 }
