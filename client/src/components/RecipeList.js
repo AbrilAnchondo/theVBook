aa078e8bc4f214b253d50d2bb76e399254ec812a
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-05-27 17:15:31 -0400

    commented out console logs

--- client/src/components/RecipeList.js
+++ client/src/components/RecipeList.js
@@ -3,9 +3,9 @@ import React from 'react';
 import Recipe from './Recipe';
 
 const RecipeList = (props) => {
-  console.log('RecipeList props:', props);
+  //console.log('RecipeList props:', props);
   const recipes = props.recipes;
-  console.log('recipes: ',recipes);
+  //console.log('recipes: ',recipes);
   return (
     <div className="grid recipe-list">
       { recipes.map(recipe => <Recipe key={recipe.id} recipe={recipe} />) }
