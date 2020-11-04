b326fb83cf6f1ae200225cdd8e8b98dfd32c5f38
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2020-11-04 15:06:50 -0600

    preparing before deployment to heroku

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+// node js module to manipulate paths
+const path = require('path');
 
 const app = express();
 
@@ -9,17 +11,23 @@ connectDB();
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
 
 //init middleware to have access to the req and res objects
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.send('API is running...'));
-
-
-
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
